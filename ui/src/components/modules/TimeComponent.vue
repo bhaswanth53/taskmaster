3eea8b22a3f08ec5150463d6d5b4e00c9ce4e0bc
bhaswanth53 <sreenivasa.b@cloudbloom.io>
2022-12-30 15:32:00 +0530

    Create time functionality

--- ui/src/components/modules/TimeComponent.vue
+++ ui/src/components/modules/TimeComponent.vue
@@ -11,13 +11,34 @@
         />
         <div class="time-divs padding-vertical" v-if="data.length > 0">
             <div class="time-logs" v-for="(time, key) in data" v-bind:key="key">
-                <div class="uk-width-1-4@s uk-grid-match" uk-grid>
-                    <div class="uk-width-1-2@s">
+                <div class="uk-grid-match uk-flex-middle" uk-grid>
+                    <div class="uk-width-1-6@s">
                         <button class="uk-button log-button">{{ time.start }}</button>
                     </div>
-                    <div class="uk-width-1-2@s">
+                    <div class="uk-width-1-6@s">
                         <button class="uk-button log-button">{{ time.end }}</button>
                     </div>
+                    <div class="uk-width-1-4@s">
+                        <p class="description" v-if="time.description">{{ time.description }}</p>
+                        <p class="description" v-else>No description!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="time-card">
+            <div class="uk-grid-match" uk-grid>
+                <div class="uk-width-1-6@s">
+                    <input type="time" class="time-input uk-input" v-model="start" placeholder="Start" />
+                </div>
+                <div class="uk-width-1-6@s">
+                    <input type="time" class="time-input uk-input" v-model="end" placeholder="end" />
+                </div>
+                <div class="uk-width-1-4@s">
+                    <input type="text" class="time-input uk-input" placeholder="description" v-model="description" />
+                </div>
+                <div class="uk-width-1-6@s">
+                    <button class="uk-button uk-button-small add-button" v-on:click="createTime">Add</button>
                 </div>
             </div>
         </div>
@@ -43,12 +64,15 @@
                 start_date: "",
                 end_date: "",
                 data: [],
-                task: ""
+                task: "",
+                start: "",
+                end: "",
+                description: ""
             }
         },
         methods: {
             getTimes() {
-                var time = Time.getTimes(false, '2022-11-01', '2022-12-11')
+                var time = Time.getTimes(false, this.start_date, this.end_date)
                 time.then((response) => {
                     this.data = response.data
                 }).catch((error) => {
@@ -71,6 +95,23 @@
             },
             handleEnd(e) {
                 this.end_date = e.target.value
+            },
+            createTime() {
+                var time = Time.addTime('', this.start, this.end, this.description)
+                time.then((response) => {
+                    if(!response.data.error) {
+                        this.getTimes()
+                        this.start = ""
+                        this.end = ""
+                        this.description = ""
+                    }
+                }).catch((error) => {
+                    if(error.response) {
+                        console.log(error.response.data)
+                    } else {
+                        console.log(error)
+                    }
+                })
             }
         }
     }
@@ -84,5 +125,25 @@
 
     .time-logs {
         margin: 10px 0px;
+    }
+
+    .time-card {
+        /* background-color: #fff; */
+        padding: 5px;
+    }
+
+    .add-button {
+        background-color: #0e3899;
+        color: #fff !important;
+        border: 0;
+        cursor: pointer;
+    }
+
+    .description {
+        background-color: #fff;
+        padding: 7px;
+        font-weight: 600;
+        font-size: 14px;
+        color: #808080;
     }
 </style>
