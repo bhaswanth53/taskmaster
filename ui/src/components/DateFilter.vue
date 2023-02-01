da07a64457bfdd5d8e3e0db6d891432e7f1c7696
bhaswanth53 <sreenivasa.b@cloudbloom.io>
2023-02-01 13:54:34 +0530

    Added date filter for task module

---
+++ ui/src/components/DateFilter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="uk-margin uk-width-1-3">
+            <label class="uk-form-label">Date :</label>
+            <input type="date"
+                    class="uk-input uk-form-small"
+                    v-on:change="handleDate"
+                    :value="date" />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DateFilter',
+        props: [
+            'date',
+            'handleDate'
+        ]
+    }
+</script>
